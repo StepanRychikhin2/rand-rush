e09e489e95d7b07cf63572f1d0075a6d8d1b48fa
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-11-16 20:33:12 +0200

    awdadad

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ import sty from './AppStyle.module.css'
 import awdad from './global.module.css'
 import Header from './components/header/header'
 const App = () => {
+	const [falling, setFalling] = useState(false)
+	const [listItem, setListItem] = useState([])
+	const handleFall = () => {
+		setFalling(true)
+		setTimeout(() => {
+			setFalling(false)
+		}, 400)
+	}
 	function chansIn(divisor) {
 		const total = 100
 		const result = Math.floor(total / divisor)
 		return result
 	}
-	const [listItem, setListItem] = useState([])
+
 	function getRandomWithChance(chances) {
+		handleFall()
 		const totalChance = chances.cards.reduce(
 			(sum, chance) => sum + chance.chans,
 			0
@@ -24,7 +33,8 @@ const App = () => {
 		for (let i = 0; i < chances.cards.length; i++) {
 			cumulativeChance += chances.cards[i].chans
 			if (random < cumulativeChance) {
-				document.getElementById('imgRund').src = chances.cards[i].photo
+				setTimeout(() => {
+					document.getElementById('imgRund').src = chances.cards[i].photo
 				document.getElementById('TitleRund').textContent =
 					chances.cards[i].namePrs
 				document.getElementById(
@@ -36,15 +46,24 @@ const App = () => {
 				document.getElementById('chansesRund').textContent = `1/${chansIn(
 					chances.cards[i].chans
 				)}`
+				}, 400)
+				
 				return setListItem([...listItem, chances.cards[i]])
 			}
 		}
 	}
+	const mystyle = falling
+		? {transform: "translateY(`300px`) rotate(45deg)",
+			 opacity: "0"}
+		: {}
+		const falg = {
+			transform: "translateY(`300px`) rotate(45deg)",
+			 opacity: "0"
+		}
 	return (
 		<div className={sty.glawBox}>
 			<Header></Header>
 			<div className={sty.boxCartRand}>
-				
 				{/* <ul>
 					{listItem.map((imt) => {
 						return (
@@ -56,23 +75,24 @@ const App = () => {
 						)
 					})}
 				</ul> */}
-				<div className={sty.cart}>
-					<img
-						className={sty.imgRund}
-						width="200px"
-						id="imgRund"
-						src="aw"
-					></img>
-					<h2 id="TitleRund"></h2>
-					<p id="AnimeNameRund"></p>
-					<p id="PointRund"></p>
-					<p id="chansesRund"></p>
+				<div id="card-container" className={sty.cart}>
+					<div style={mystyle} className={sty.card}>
+						<img
+							className={sty.imgRund}
+							width="200px"
+							id="imgRund"
+							src="aw"
+						></img>
+						<h2 id="TitleRund"></h2>
+						<p id="AnimeNameRund"></p>
+						<p id="PointRund"></p>
+						<p id="chansesRund"></p>
+					</div>
 				</div>
-				<div >
+				<div>
 					<button onClick={() => getRandomWithChance(dataCard)}>rand</button>
 				</div>
 			</div>
-			
 		</div>
 	)
 }
