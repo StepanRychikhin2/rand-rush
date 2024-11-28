0542f99a199e44e337e414a0affc4db30238015a
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-11-28 21:08:43 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg'
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { nanoid } from 'nanoid'
 import dataCard from './dataCard.json'
 import sty from './AppStyle.module.css'
 import awdad from './global.module.css'
 import Header from './components/header/header'
 const App = () => {
+	const [socondrender, setSocondrender] = useState(true)
+	const [caedkist, setCaedkist] = useState([])
 	const [falling, setFalling] = useState(false)
+	const [invVis, setInvVis] = useState(false)
 	const [listItem, setListItem] = useState([])
+	useEffect(() => {
+		if (socondrender) {
+			setSocondrender(false)
+			setCaedkist(dataCard.cards)
+		}
+	})
 	const handleFall = () => {
 		setFalling(true)
 		setTimeout(() => {
@@ -22,47 +31,49 @@ const App = () => {
 	}
 
 	function getRandomWithChance(chances) {
+		const btnRundRush = document.getElementById('btnRund')
+		btnRundRush.setAttribute('disabled', 'disabled')
+
+		setTimeout(() => {
+			btnRundRush.removeAttribute('disabled')
+		}, 1000)
+
 		handleFall()
-		const totalChance = chances.cards.reduce(
-			(sum, chance) => sum + chance.chans,
-			0
-		)
+		const totalChance = chances.reduce((sum, chance) => sum + chance.chans, 0)
 		const random = Math.random() * totalChance
 		let cumulativeChance = 0
 
-		for (let i = 0; i < chances.cards.length; i++) {
-			cumulativeChance += chances.cards[i].chans
+		for (let i = 0; i < chances.length; i++) {
+			cumulativeChance += chances[i].chans
 			if (random < cumulativeChance) {
 				setTimeout(() => {
-					document.getElementById('imgRund').src = chances.cards[i].photo
-				document.getElementById('TitleRund').textContent =
-					chances.cards[i].namePrs
-				document.getElementById(
-					'AnimeNameRund'
-				).textContent = `Anime name: ${chances.cards[i].AnimeName}`
-				document.getElementById(
-					'PointRund'
-				).textContent = `points: ${chances.cards[i].point}`
-				document.getElementById('chansesRund').textContent = `1/${chansIn(
-					chances.cards[i].chans
-				)}`
+					document.getElementById('imgRund').src = chances[i].photo
+					document.getElementById('TitleRund').textContent = chances[i].namePrs
+					document.getElementById(
+						'AnimeNameRund'
+					).textContent = `Anime name: ${chances[i].AnimeName}`
+					document.getElementById(
+						'PointRund'
+					).textContent = `points: ${chances[i].point}`
+					document.getElementById('chansesRund').textContent = `1/${chansIn(
+						chances[i].chans
+					)}`
 				}, 400)
-				
-				return setListItem([...listItem, chances.cards[i]])
+
+				return setListItem([...listItem, chances[i]])
 			}
 		}
 	}
 	const mystyle = falling
-		? {transform: "translateY(`300px`) rotate(45deg)",
-			 opacity: "0"}
+		? { transform: 'translateY(`300px`) rotate(45deg)', opacity: '0' }
 		: {}
-		const falg = {
-			transform: "translateY(`300px`) rotate(45deg)",
-			 opacity: "0"
-		}
+	const falg = {
+		transform: 'translateY(`300px`) rotate(45deg)',
+		opacity: '0',
+	}
 	return (
 		<div className={sty.glawBox}>
-			<Header></Header>
+			<Header setInvVis={setInvVis} invVis={invVis}></Header>
 			<div className={sty.boxCartRand}>
 				{/* <ul>
 					{listItem.map((imt) => {
@@ -75,6 +86,17 @@ const App = () => {
 						)
 					})}
 				</ul> */}
+				<ul style={{ display: 'none' }}>
+					{caedkist.map((imt) => {
+						return (
+							<li key={nanoid()}>
+								<p>chanse {imt.chans}%</p>
+								<p>txt{imt.txt}</p>
+								<img width="200px" src={imt.photo}></img>
+							</li>
+						)
+					})}
+				</ul>
 				<div id="card-container" className={sty.cart}>
 					<div style={mystyle} className={sty.card}>
 						<img
@@ -90,9 +112,18 @@ const App = () => {
 					</div>
 				</div>
 				<div>
-					<button onClick={() => getRandomWithChance(dataCard)}>rand</button>
+					<button id="btnRund" onClick={() => getRandomWithChance(caedkist)}>
+						rand
+					</button>
 				</div>
 			</div>
+			{invVis && (
+				<div className={sty.invBoxModalBackdrop}>
+					<div>
+						<h1>hi</h1>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,20 @@
 import sty from "./Header.module.css"
-const Header = () => {
+const Header = (setData,data) => {
+//     console.log(data)
+//     console.log(setData)
+// function change() {
+//     setData(true)
+// }
+
     return <div className={sty.headerbox}>
-<h2 className={sty.title}>Rund Rush</h2>
+        <div>
+        <h2 className={sty.title}>Rund Rush</h2>
+
+        </div>
+        <button className={sty.invetoryBtn}>
+            <h3 id="invent" className={sty.invetoryTitle}>inventory</h3>
+        </button>
+
     </div>
 }
 
